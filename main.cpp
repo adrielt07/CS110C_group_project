d3241cc34f409102c54a0954a1f592eeb6b24980
kellenzheng <105254154+kellenzheng@users.noreply.github.com>
2022-05-23 20:18:29 -0700

    kellen patch (#2)
    * kellen patch
    
    * private:
    -added "double SD" as a private member
    
    public:
    GETTERS:
    -added get_SD() as a getter to SD
    -minor fix to get_max() to actually return max
    -added length_total() getter function //returns total # elements
    -added length_unique() function //returns # unique elements
    -added unique_set() function //returns a set of unique elements from a dataset
    -added search() function
    
    MUTATORS:
    -added calc_SD() to calculate SD value into SD member
    
    HELPERS:
    -changed "Size" in check_data -> "Length: " and uses length_total() getter function (done to satisfy the function requirement)
    -added sqrt() function to find the square root of a number (since we can't use math.h)
    -added print_unique() function to print the unique values of a data set
    -updated check_data & update_data to also show SD and unique elements
    
    main:
    -added more values to the data sets to test for duplicates
    -added function call to test search()
    
    * Delete kzheng13

--- main.cpp
+++ main.cpp
@@ -18,29 +18,37 @@ int main()
 
     // **** TEST CONSTRUCTOR FOR VECTOR AND ARRAY **** //
     // Test an array
-    int size = 5;
-    double arr_double [size] = { 16.5, 2.2, 77.9, 40.15, 30 }; 
+    int size = 10;
+    double arr_double [size] = { 16.5, 2.2, 77.9, 40.15, 30, 40.15, 52, 39, 16.5, 16.5 };
     SimpleStat<double> l2(arr_double, size);
     std::cout << "\n< Check l2 data >" << std::endl;
     l2.check_data();
+    l2.search(16.5);
 
     //  Test vector
     std::vector<double> vect;
     vect.push_back(10.1);
     vect.push_back(20.2);
     vect.push_back(30.5);
+    vect.push_back(15.3);
+    vect.push_back(20.2);
     SimpleStat<double> l3(vect);
     std::cout << "\n< Check l3 data >" << std::endl;
     l3.check_data();
+    l3.search(20.2);
 
     // Test Set
     std::set<double> s;
     s.insert(4.3);
     s.insert(5.5);
     s.insert(5.10);
+    s.insert(5.5);
+    s.insert(4.3);
+    s.insert(6.1);
     SimpleStat<double> l4(s);
     std::cout << "\n< Check l4 data >" << std::endl;
     l4.check_data();
+    l4.search(10);
 
     std::cout << "\n< Check l4 data after running empty() >" << std::endl;
     l4.empty();
